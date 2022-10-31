9ba8d47c00b9cf9fb38094d74a918593af9d0765
adexfox <adewale199@gmail.com>
2022-11-01 00:24:35 +0100

    :closed_lock_with_key: Final Details

--- src/item.jsx
+++ src/item.jsx
@@ -9,37 +9,43 @@ const item =  [
     {   
         id: 'twitter',
         text: 'Twitter link', 
-        url: 'https://twitter.com/Adexfox20'
+        url: 'https://twitter.com/Adexfox20',
+        title: 'Twitter Handle'
        
     },
     {   
         id: 'btn__zuri',
         text: 'Zuri Team', 
-        url: 'https://training.zuri.team/'
+        url: 'https://training.zuri.team/',
+        title: 'No one does it like us'
        
     },
     {   
         id: 'books',
         text: 'Zuri Books', 
-        url: 'http://books.zuri.team'
+        url: 'http://books.zuri.team',
+        title: 'Get career and life changing books'
        
     },
     {   
         id: 'book__python',
         text: 'Python Books', 
-        url: 'https://books.zuri.team/python-for-beginners?ref_id=Adexfox'
+        url: 'https://books.zuri.team/python-for-beginners?ref_id=Adexfox',
+        title: 'Python library like no other'
        
     },
     {   
         id: 'pitch',
         text: 'Background Check for Coders', 
-        url: 'https://background.zuri.team'
+        url: 'https://background.zuri.team',
+        title: 'Vetting for top talents around the world'
        
     },
     {   
         id: 'book__design',
         text: 'Design Books', 
-        url: 'https://books.zuri.team/design-rules'
+        url: 'https://books.zuri.team/design-rules',
+        title: 'The Journey to Design World'
        
     },
     // {   
--- src/profile.jsx
+++ src/profile.jsx
@@ -1,34 +1,58 @@
 import React from 'react';
 import item from './item.jsx';
-import { FaSlack, FaGithub } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { IconContext } from 'react-icons/lib/esm/iconContext';
+import { RiShareForwardLine } from "react-icons/ri";
+import { BsThreeDots } from "react-icons/bs";
 
 const Profile = () => {
 
   return (
-    <IconContext.Provider value={{ style: {marginRight:  '0.5rem'}, className: 'social'}}>
+    <IconContext.Provider value={{ size: '1.40rem', style: {marginRight:  '0.5rem'}, className: 'social'}}>
         <main>
 
             {item && item.map(( props )=>{
-                const { id, text, url } = props;
+                const { id, text, url, title } = props;
                 return(
                     <div className='cta'>
-                        <a href={url}>
+                        <a href={url} id={id} title ={title}>
 
-                            <button className='btn' id={id}>{ text }</button>
+                            <button className='btn'>{ text }</button>
 
                         </a>
                     </div>
                 );
             })}
-            <header>
-                <div id='profile__img'></div>
-                <h3>Adewale Adeola</h3>
+            <section className='profile-section'>
+                <div id='profile__img'>
+                    <div className='overlayer'>
+                        <img src="images/cam.svg" alt="" className='cam'/>
+                    </div>
+                </div>
+                <h3 id='twitter'>@Adexfox20</h3> 
 
-            </header>
+                   { /******************************
+                      SLACK USERNAME === ADEXFOX 
+                     ******************************/}
+                {/* <h3 id='slack'>Adexfox</h3> */}
+
+                <RiShareForwardLine className= 'share-1' size= '2rem'/>
+                <BsThreeDots className= 'share-2' size= '2rem'/>
+
+            </section>
+            <div className='icons'>
+                <a href='/' className='icon-1'>
+
+                    <img src="images/slack.svg" alt=""/>
+                </a>
+                <a href="https://github.com/Adexfox">
+                    <FaGithub />
+                </a>
+            </div>
             <footer>
-                <FaSlack />
-                <FaGithub />
+                <img src="images/zuri.svg" alt="" />
+                <h3>HNG Internship 9 Frontend Task</h3>
+                <img src="images/14g.svg" alt="" />
             </footer>
         </main>
     </IconContext.Provider>
